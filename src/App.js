a21a443c606d6f77d9937972a95fb738c4e753ae
Elwerco <alexsteev2002@gmail.com>
2018-07-06 10:56:33 -0700

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './components/Form';
+
+function App() {
+	return (
+		<div className="root1">
+			<h1 className="hAdd">Add</h1>
+			<Form />
+		</div>
+	)
+}
+
+export default App;
