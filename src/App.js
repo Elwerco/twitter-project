fcd712ea24be7bb88f10fe27d49a00bd70ce6c28
Elwerco <alexsteev2002@gmail.com>
2018-07-24 11:42:09 -0700

    sdfwef

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+        localStorage.setItem('myKey', JSON.stringify(persons));
+        alert("sdf");
+      })
+  }
 
   render() {
     return (
       <div>
-         {/*<Nav />*/}
-      	 {/*<Add />*/}
- 		  {/*   <Routing />*/}
       </div>
     );
   }
